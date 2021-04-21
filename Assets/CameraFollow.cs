b174e668217dfaaf0e16a5dadfe8692faddd8cbc
Tobias Christensen <Tobias@damsboe.dk>
2021-04-21 20:01:27 +0200

    Camera follow
    Cameraet følger ny efter dronen

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour  {
+   
+    public Transform Drone;
+
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+
+    // LateUpdate er det samme som update den gør det bare efter update.
+    void LateUpdate()
+    {
+        Vector3 desiredPosition = Drone.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+    }
+}
+
