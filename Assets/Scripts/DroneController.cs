524743421414b3adbb1c65aedf6aa880b84eb37d
MathiasYde <35201354+MathiasYde@users.noreply.github.com>
2021-04-21 08:41:13 +0200

    Imported (almost) blank project

---
+++ Assets/Scripts/DroneController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneController : MonoBehaviour {
+    public float height { get; private set; }
+
+    void Update() {
+        (bool success, RaycastHit hit) = Raycast();
+        if (success) {
+            height = hit.distance;
+        }
+
+
+    }
+
+    (bool, RaycastHit) Raycast() {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, -transform.up, out hit, Mathf.Infinity))
+        {
+            Debug.DrawRay(transform.position, -transform.up * hit.distance, Color.yellow);
+            return (true, hit);
+        }
+        return (false, hit);
+    }
+}
